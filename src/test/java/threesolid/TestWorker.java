ea86aaefcdd336cd67d259f6c6df33246fb32ec6
Marchundia <42947017+Marchundia@users.noreply.github.com>
2018-10-28 01:23:46 -0500

    Adding in assertNotEquals test to TestWorker
    Testing strings from worker to determine consistent output and gradle testing.

--- src/test/java/threesolid/TestWorker.java
+++ src/test/java/threesolid/TestWorker.java
@@ -12,15 +12,28 @@ public class TestWorker {
 		assertEquals("I'm working already!", w.workString(), "Mismatch between test text and method text");
 	}
 
+
 	@Test
 	@DisplayName("Stop Working Method Test")
 	public void newtestZSWorkerPass() {
 		assertEquals("I am no longer working. Whew.", w.stopWorking(), "Worker should stop working.");
 	}
-
 	@Test
 	@DisplayName("Keep Working Method Test")
 	public void newtestZSWorkerFail() {
 	       assertEquals("I'm still working...", w.keepWorking(), "Worker should keep working.");
+	}
+
+
+	@Test
+	@DisplayName("Stop Working Method Test assertNotEquals")
+	public void newtestMAWorkerPass() {
+		assertNotEquals("Hey, hey ,hey!", w.stopWorking(), "Worker should return differently.");
+	}
+
+	@Test
+	@DisplayName("Keep Working Method Test assertNotEquals")
+	public void newtestMAWorkerFail() {
+		assertNotEquals("I don't want to! I quit!", w.keepWorking(), "Worker should return differently.");
 	}
 }
